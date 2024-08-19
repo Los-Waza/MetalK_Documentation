eab9cd005a3b96194d118e300a78424e1051d089
Santher <erickhernandop@gmail.com>
2024-08-19 14:37:33 -0500

    Data Base Structured

--- ModeloDeDatos/metalK_dataBase.sql
+++ ModeloDeDatos/metalK_dataBase.sql
@@ -4,25 +4,23 @@ CREATE DATABASE  MetalK_WebCenter;
 USE MetalK_WebCenter;
 
 -- DECLARE TABLES
-DROP TABLE IF EXISTS solicitudes;
 CREATE TABLE solicitudes(
 	soli_id INT NOT NULL,
     soli_date INT NOT NULL,
     soli_plane VARCHAR(2843) NOT NULL,
     soli_technicalVisit BOOLEAN NOT NULL,
     soli_address VARCHAR(2843) NOT NULL,
-    clin_id INT NOT NULL
+    clie_id INT NOT NULL
 );
 
- DROP TABLE IF EXISTS products;
  CREATE TABLE products(
 	prod_id INT NOT NULL ,
     prod_price DOUBLE NOT NULL,
     prod_time INT NOT NULL, 
-    cate_id INT NOT NULL
+    cate_id INT NOT NULL,
+    soli_id INT NOT NULL
  );
 
-DROP TABLE IF EXISTS categories;
 CREATE TABLE categories(
 	cate_id INT NOT NULL,
     cate_name VARCHAR(32) NOT NULL,
@@ -30,7 +28,6 @@ CREATE TABLE categories(
     cate_icon VARCHAR(2843) NOT NULL
 );
 
-DROP TABLE IF EXISTS tags;
 CREATE TABLE tags(
 	tag_id INT NOT NULL ,
     tag_name VARCHAR(32) NOT NULL,
@@ -38,7 +35,6 @@ CREATE TABLE tags(
     tagg_id INT NOT NULL
 );
 
-DROP TABLE IF EXISTS tag_groups;
 CREATE TABLE tag_groups  (
 	tagg_id INT NOT NULL ,
     tagg_title VARCHAR(32) NOT NULL,
@@ -46,35 +42,30 @@ CREATE TABLE tag_groups  (
     tagg_icon VARCHAR(2843) NOT NULL
 );
 
-DROP TABLE IF EXISTS tags_by_cartegory;
-CREATE TABLE  tags_by_cartegory(
+CREATE TABLE  tags_by_category(
 	tbca_id INT NOT NULL ,
     cate_id INT NOT NULL,
     tag_id INT NOT NULL
 );
 
-DROP TABLE IF EXISTS product_tags;
 CREATE TABLE product_tags(
 	prta_id INT NOT NULL ,
     prod_id INT NOT NULL,
 	tag_id  INT NOT NULL
 );
 
-DROP TABLE IF EXISTS product_pictures;
 CREATE TABLE product_pictures(
 	prpi_id INT NOT NULL ,
     prpi_route INT NOT NULL,
 	prod_id  INT NOT NULL
 );
 
-DROP TABLE IF EXISTS clients;
 CREATE TABLE clients(
-	clin_id INT NOT NULL ,
-    clin_name VARCHAR(64) NOT NULL,
-	clin_phone  VARCHAR(16) NOT NULL
+	clie_id INT NOT NULL ,
+    clie_name VARCHAR(64) NOT NULL,
+	clie_phone  VARCHAR(16) NOT NULL
 );
 
-DROP TABLE IF EXISTS admins;
 CREATE TABLE admins(
 	admi_id INT NOT NULL ,
     admi_name VARCHAR(64) NOT NULL,
@@ -82,7 +73,6 @@ CREATE TABLE admins(
     admi_password VARCHAR(256) NOT NULL
 );
 
-DROP TABLE IF EXISTS enterprice;
 CREATE TABLE enterprice(
 	ente_id INT NOT NULL ,
     ente_name VARCHAR(64),
@@ -90,19 +80,85 @@ CREATE TABLE enterprice(
     ente_description VARCHAR(256),
     ente_logo VARCHAR(2843),
     ente_address VARCHAR(256)
-);
+); 
 
 -- ADD CONSTRINTS
 -- PRIMARY KEYS
 ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
 ALTER TABLE products MODIFY COLUMN prod_id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE solicitudes MODIFY COLUMN soli_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE clients MODIFY COLUMN clie_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE product_pictures MODIFY COLUMN prpi_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE product_tags MODIFY COLUMN prta_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE categories MODIFY COLUMN cate_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE tags_by_category MODIFY COLUMN tbca_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE tags MODIFY COLUMN tag_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE tag_groups MODIFY COLUMN tagg_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE enterprice MODIFY COLUMN ente_id INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE admins MODIFY COLUMN admi_id INT AUTO_INCREMENT PRIMARY KEY;
 
 -- FOREING KEYS 
-ALTER TABLE solicitudes ADD CONSTRAINT FOREIGN KEY (clin_id) REFERENCES clients(clin_id)
+ALTER TABLE solicitudes ADD CONSTRAINT FOREIGN KEY (clie_id) REFERENCES clients(clie_id);
+ALTER TABLE products ADD CONSTRAINT FOREIGN KEY (soli_id) REFERENCES solicitudes(soli_id);
+ALTER TABLE products ADD CONSTRAINT FOREIGN KEY (cate_id)  REFERENCES categories(cate_id);
+ALTER TABLE product_pictures ADD CONSTRAINT FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+ALTER TABLE product_tags ADD CONSTRAINT FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+ALTER TABLE product_tags ADD CONSTRAINT FOREIGN KEY (tag_id) REFERENCES tags(tag_id);
+ALTER TABLE tags_by_category ADD CONSTRAINT FOREIGN KEY (cate_id) REFERENCES categories(cate_id);
+ALTER TABLE tags_by_category ADD CONSTRAINT FOREIGN KEY (tag_id) REFERENCES tags(tag_id);
+ALTER TABLE tags ADD CONSTRAINT FOREIGN KEY (tagg_id) REFERENCES tag_groups(tagg_id);
+ 
+-- CRUD clients
+DELIMITER //
+CREATE PROCEDURE createClient(name VARCHAR(36),cellphone VARCHAR(16))
+BEGIN
+	INSERT INTO clients(clie_name,clie_phone)
+    VALUES (name,cellphone);
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE readAllClients()
+BEGIN
+	SELECT *
+	FROM clients;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE updateClients(IN id INT,IN name VARCHAR(64),IN cellphone VARCHAR(16))
+BEGIN
+	-- IF id = NULL THEN
+      
+    -- END IF;
+	IF name != NULL THEN
+		UPDATE clients c 
+		SET c.clie_name = name 
+		WHERE c.clie_id = id;
+    END IF;
+	IF cellphone != NULL THEN
+		UPDATE clients c 
+		SET c.clie_phone = cellphone 
+		WHERE c.clie_id = id;
+    END IF;
+END;
+// DELIMITER ;
+-- CALL createClient("Salomon",345673892);
+-- CALL updateClients(1,"Juan",NULL);
+
+DELIMITER //
+CREATE PROCEDURE  deleteClient(id INT)
+BEGIN
+	DELETE FROM clients c WHERE c.id = id; 
+END;
+// DELIMITER ;
+
+-- CRUD solicitude
+DELIMITER //
+CREATE PROCEDURE createSolicutude()
+BEGIN
+END;
+// DELIMITER ;
+  
+
 
 
